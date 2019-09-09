66a38b80dfae202d94177e6b4c4fa5991ff940e9
Samer <samerxxxalsudani@gmail.com>
2019-09-09 10:22:07 -0500

    Second Initial

---
+++ build/precache-manifest.f5deaf5092b6513985d83a9d061b8cf5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "a791bb0a9605b45c7205ff8fdae6686d",
+    "url": "/static/media/action-1867700_1280.a791bb0a.jpg"
+  },
+  {
+    "revision": "82b291d23b22dd21aab0",
+    "url": "/static/css/main.cd040355.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "813252df7a413a734af47f8c101a35ad",
+    "url": "/static/media/1-1.813252df.jpeg"
+  },
+  {
+    "revision": "cdb437de5f6303c7576e",
+    "url": "/static/js/2.7f35d49d.chunk.js"
+  },
+  {
+    "revision": "492f1aa348880e13cfadd7cb3a892d25",
+    "url": "/static/media/Shoe-Carnival-1080x720.492f1aa3.jpg"
+  },
+  {
+    "revision": "984874d19d1cdef8dcee5fcea55c082d",
+    "url": "/static/media/mainBcg.984874d1.jpeg"
+  },
+  {
+    "revision": "82b291d23b22dd21aab0",
+    "url": "/static/js/main.f2caf589.chunk.js"
+  },
+  {
+    "revision": "945afb997e0731b038ed58fadacfeb3f",
+    "url": "/static/media/contactBcg.945afb99.jpeg"
+  },
+  {
+    "revision": "9675841294261b773267d557f531a8ff",
+    "url": "/static/media/singleProductBcg.96758412.jpeg"
+  },
+  {
+    "revision": "46143c2f25bc4667fac18f5924288217",
+    "url": "/static/media/storeBcg.46143c2f.jpeg"
+  },
+  {
+    "revision": "0819a81634391f8518fe325378b8f994",
+    "url": "/static/media/defaultBcg.0819a816.jpeg"
+  },
+  {
+    "revision": "cdb437de5f6303c7576e",
+    "url": "/static/css/2.56cf5b79.chunk.css"
+  },
+  {
+    "revision": "c8c97c13fc106425256597d486be30ca",
+    "url": "/index.html"
+  }
+];
